2e659bb795624f457a2721943ec14ec3aeb447e3
Fael Cm <rafaacintramartins@gmail.com>
2022-12-07 22:18:40 -0300

    Itinializing project

---
+++ src/routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export function InitialRoutes() {
+  return (
+        <BrowserRouter>
+            <Routes>
+                <Route></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
